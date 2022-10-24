53c42e44f60c261d64f9661d1b8746e793c78bb5
MuhamadRizky14 <203040043@mail.unpas.ac.id>
2022-10-24 14:12:47 +0700

    Menyelesaikan video 6 Database, Migration & Eloquent

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "name" => "Muhamad Rizky",
+        "email" => "203040043@unpas.ac.id",
+        "image" => "profile.jpg"
+    ]);
+});
+
+Route::get('/blog', [PostController::class, 'index']);
+// Halaman Single Post
+Route::get('posts/{slug}', [PostController::class, 'show']);
