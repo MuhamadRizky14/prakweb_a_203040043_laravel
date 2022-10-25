168c179ce9b5920f947ad7e853417907b6192e22
MuhamadRizky14 <203040043@mail.unpas.ac.id>
2022-10-25 12:43:18 +0700

    Menyelesaikan video 7 Post Model

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,11 @@
 
 @section('container')
 <article>
-    <h2>{{ $post["title"] }}</h2>
-    <h5>{{ $post["author"] }}</h5>
-    <p>{{ $post["body"] }}</p>
+    <h1>{{ $post->title }}</h1>
+
+{!! $post->body !!}
+
 </article>
 <a href="/blog">Back to posts</a>
 @endsection
+
