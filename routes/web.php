e9fee2ee1ea499c76abf6e68560887db11b8a2d3
MuhamadRizky14 <203040043@mail.unpas.ac.id>
2022-11-01 18:07:58 +0700

    Menyelesaikan video 14 View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -55,9 +55,13 @@ Route::get('categories/{category:slug}', function(Category $category) {
 });
 
 
-Route::get('/author/{author:username}', function(User $author){
+Route::get('/author/{author:username}', function($author){
     return view('posts', [
         'title' => "Post By Author : $author->name",
         'posts' => $author->posts->load('category', 'author'),
     ]);
 });
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
